7748923e2c9ebd7d6c1ced2c82015fde5574c0fc
Sheng Cruz <vansx001@gmail.com>
2016-10-01 13:24:10 -0500

    updated results display in html/js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
             dataType: "jsonp",
             data: {
                 term: terms,
-                limit: 25,
+                limit: 25
             },
             success: function (search) {
                 $('#itunes-results').children().remove();
@@ -21,29 +21,28 @@ $(document).ready(function () {
             }
         });
 
-        function displayResults(search) {
-            var results = search.results;
-            var index = ''; //results will show up on the index page! 
+    function displayResults(search) {
+        var results = search.results;
+        var index = ''; //results will show up on the index page! 
             for (var i = 0; i < results.length; i++) {
                 var item = results[i];
-                index += '<div class="search-results">';
-                index += '<img class="thumbnail" src="' + item.artworkUrl100 + '"/>';
-                index += '<h3>' + item.artistName + ' - ' + item.trackName + '</h3>';
+                index += '<li class="results-list" id="search-results">';
+                index += '<img class="img-rounded img-responsive img-hover" src="' + item.artworkUrl100.replace('100x100','600x600') + '"/><br>';
+                index += '<h3>' + item.artistName + ' - ' + item.trackName + '</h3><br>';
                 if (item.collectionName !== undefined) {
-                    index += '<h4>Album: ' + item.collectionName + '</h4>';
-                    index += '<h4>Genre: ' + item.primaryGenreName + '</h4>';
+                    index += '<h4>Album: ' + item.collectionName + '</h4><br>';
+                    index += '<h4>Genre: ' + item.primaryGenreName + '</h4><br>';
                     index += '<audio src="' + item.previewUrl + '"controls></audio><br>';
-                    index += '</div>';
+                    
                 } else {
                     index += '<h4>Album: N/A </h4>';
                 }
-                $('#itunes-results').html(index); //these codes will go to the #itunes-results div 
+                index += '</li>';
+                $('#itunes-results').html(index); //displays results in the #itunes-results div 
             }
-        };
+        }
 
-        $("#lightening").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100)
-            .fadeIn(100).fadeOut(100).fadeIn(100);
-        
+        $("#lightening").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
         // $('#listitems').paginate({itemsPerPage: 10});
     }
 });
